7adf62c2f5192bc0765715ac34c7ec359f4224f0
jebatista <batista-sk@hotmail.com>
2025-10-21 16:44:07 +0900

    fix

---
+++ generateSBATCH.py
@@ -0,0 +1,9 @@
+
+pb = ['jacobi-1d', 'bicg', 'atax', 'gesummv', 'trisolv', 'durbin', 'mvt', 'gemver', 'deriche', 'doitgen', 'gemm', 'syrk', '2mm', 'trmm', 'symm', 'jacobi-2d', 'fdtd-2d', 'cholesky', 'syr2k', '3mm', 'correlation', 'covariance', 'heat-3d', 'gramschmidt', 'ludcmp', 'lu', 'nussinov', 'adi', 'floyd-warshall', 'seidel-2d']
+    
+
+for i in range(len(pb)):
+    if i == 0:
+        print("jid%d=$(sbatch -o results_evost/%s.txt genoa_EvoTADASHI_Heuristic_new.sh --benchmark %s | aqk '{print $4}')" % (i, pb[i], pb[i]) )
+    else:
+        print("jid%d=$(sbatch -o results_evost/%s.txt --dependency=afterany:$jid%d genoa_EvoTADASHI_Heuristic_new.sh --benchmark %s | aqk '{print $4}')" % (i, pb[i], i-1, pb[i]) )
