7b1ab2e43a4c193f72608e9c7d4cfe01a51e9a66
jebatista <batista-sk@hotmail.com>
2025-10-23 16:27:59 +0900

    fix

--- EvoTADASHI.py
+++ EvoTADASHI.py
@@ -565,7 +565,7 @@ if __name__ == "__main__":
             print("\n\n\n")
             parser = argparse.ArgumentParser()
             parser.add_argument("--benchmark", type=str, default=benchmark)
-            parser.add_argument("--dataset", type=str, default="EXTRALARGE")
+            parser.add_argument("--dataset", type=str, default="LARGE")
             parser.add_argument("--oflag", type=int, default=3)
             parser.add_argument("--seed", type=int, default=47)
             parser.add_argument("--population-size", type=int, default=50)
