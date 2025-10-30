8458cbc44cc87cb183ee13be044ba1dc130352c7
jebatista <batista-sk@hotmail.com>
2025-10-30 11:04:16 +0900

    clean up

---
+++ main.py
@@ -0,0 +1,65 @@
+
+
+import argparse
+from BeamSearch import BeamSearch
+from EvoTADASHI import EvoTADASHI
+
+ 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--benchmark", type=str, default="jacobi-1d")
+    parser.add_argument("--dataset", type=str, default="LARGE")
+    parser.add_argument("--oflag", type=int, default=3)
+    parser.add_argument("--seed", type=int, default=47)
+    parser.add_argument("--n-trials", type=int, default=2)
+    parser.add_argument("--n-threads", type=int, default=1)
+    parser.add_argument("--method", type=str, default="EvoTADASHI")
+    args = parser.parse_args()
+
+    if args.method == "EvoTADASHI":
+    	method = EvoTADASHI()
+    elif args.method == "BeamSearch":
+    	method = BeamSearch()
+
+    if args.benchmark == "all":
+        pb = [
+            "jacobi-1d",
+            "bicg",
+            "atax",
+            "gesummv",
+            "trisolv",
+            "durbin",
+            "mvt",
+            "gemver",
+            "deriche",
+            "doitgen",
+            "gemm",
+            "syrk",
+            "2mm",
+            "trmm",
+            "symm",
+            "jacobi-2d",
+            "fdtd-2d",
+            "cholesky",
+            "syr2k",
+            "3mm",
+            "correlation",
+            "covariance",
+            "heat-3d",
+            "gramschmidt",
+            "ludcmp",
+            "lu",
+            "nussinov",
+            "adi",
+            "floyd-warshall",
+            "seidel-2d",
+        ]
+        pb = pb[:]
+        for benchmark in pb:
+            print("\n\n\n")
+            args.benchmark = benchmark
+            print(str(args) + "\n")
+            method.fit(args)
+    else:
+        print(str(args) + "\n")
+        method.fit(args)
---
+++ util.py
@@ -0,0 +1,95 @@
+
+
+def random_args(node, tr):
+    """
+    Get random arguments for the transformation
+    Tiling is done using a number from [8, 16, 32, 64]
+    Other transformation use a random number from -64 to 64
+    """
+    tiles = [TrEnum.TILE1D, TrEnum.TILE2D, TrEnum.TILE3D]
+    if tr in tiles:
+        tile_size = choice([2**x for x in range(3, 7)])
+        return [tile_size] * (1 + tiles.index(tr))
+    return choice(node.get_args(tr, start=-64, end=64))
+
+def getAllPossible(app):
+    scops = app.scops
+    tmp = []
+    for si in range(len(scops[0].schedule_tree)):
+        s = scops[0].schedule_tree[si]
+        av = s.available_transformations
+        tmp.append( (si, av) )
+
+    ret = []
+    for x2, trans in tmp:
+        for tran in trans:
+            ret.append([x2, tran])
+    return ret
+
+
+def isLegal(app, nextStep):
+    scop = app.scops[0]
+    valid = -1
+    try:
+        valid = scop.transform_list([nextStep])
+        tapp = app.generate_code()
+        tapp.compile()
+        scop.schedule_tree[nextStep[0]].rollback()
+        # At least one operation is not valid
+        return sum([0 if v else 1 for v in valid]) == 0
+    except ValueError:
+        return False
+    except:
+        if valid != -1:
+            scop.schedule_tree[nextStep[0]].rollback()
+        # If it cant transform, its not valid
+        return False
+
+def generateAndCompile(app, op_list):
+    app = app.generate_code(populate_scops=True)
+    app.reset_scops()
+    scop = app.scops[0]
+    valid = -1
+    try:
+        valid = scop.transform_list(op_list)
+        tapp = app.generate_code()
+        tapp.compile()
+        # At least one operation is not valid
+        return tapp
+    except ValueError:
+        return False
+    except:
+        return False
+
+
+def evaluate(app, n_trials, timeout):
+    evals = []
+    for _ in range(n_trials):
+        try:
+            evals.append(app.measure(timeout=timeout))
+        except TimeoutExpired:
+            # If the evaluations takes too long, it gets a bad fitness
+            evals.append(timeout)
+
+    # multiplied by -1 so fitness is meant to be maximized
+    return -1 * min(evals) 
+
+
+
+
+def evaluateList(app_factory, op_list, n_trials=2, timeout = 99):
+    app = app_factory.generate_code(populate_scops=True)
+    scop = app.scops[0]
+    scop.transform_list(op_list)
+    app.compile()
+    return evaluate(app, n_trials, timeout)
+
+
+
+def multiProcess_evaluation(a):
+    app, n_trials, timeout = a
+    
+    if not app:
+        return "E"
+
+    return evaluate(app, n_trials, timeout)
