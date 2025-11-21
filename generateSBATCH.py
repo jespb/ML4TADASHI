d0e56624c62fc41d54499253050e96a181e1661a
jebatista <batista-sk@hotmail.com>
2025-11-21 13:58:28 +0900

    generate batch

--- generateSBATCH.py
+++ generateSBATCH.py
@@ -11,13 +11,22 @@ for i in range(len(pb)):
 
 print("\n\n\n")
 
-for i in range(len(pb)):
-    #t = (pb[i], pb[i], pb[i])
-    if i == 0:
-        print("jid%d=$(sbatch -o results_evost_XL/Evo_B_%s.txt --job-name=%s genoa_ML4T.sh --benchmark %s --dataset EXTRALARGE --method EvoTADASHI --use-heuristic| awk '{print $4}')" % (i*3,pb[i], pb[i], pb[i]))
-        print("jid%d=$(sbatch -o results_evost_XL/Evo_H_%s.txt --job-name=%s genoa_ML4T.sh --benchmark %s --dataset EXTRALARGE --method EvoTADASHI| awk '{print $4}')" % (i*3+1,pb[i], pb[i], pb[i]))
-        print("jid%d=$(sbatch -o results_evost_XL/BeamS_%s.txt --job-name=%s genoa_ML4T.sh --benchmark %s --dataset EXTRALARGE --method BeamSearch| awk '{print $4}')" % (i*3+2,pb[i], pb[i], pb[i]))
-    else:
-        print("jid%d=$(sbatch -o results_evost_XL/Evo_B_%s.txt --job-name=%s --dependency=afterany:$jid%d genoa_ML4T.sh --benchmark %s --dataset EXTRALARGE --method EvoTADASHI --use-heuristic| awk '{print $4}')" % (i*3  ,pb[i], pb[i],i*3-3, pb[i]))
-        print("jid%d=$(sbatch -o results_evost_XL/Evo_H_%s.txt --job-name=%s --dependency=afterany:$jid%d genoa_ML4T.sh --benchmark %s --dataset EXTRALARGE --method EvoTADASHI| awk '{print $4}')"                 % (i*3+1,pb[i], pb[i],i*3-2, pb[i]))
-        print("jid%d=$(sbatch -o results_evost_XL/BeamS_%s.txt --job-name=%s --dependency=afterany:$jid%d genoa_ML4T.sh --benchmark %s --dataset EXTRALARGE --method BeamSearch| awk '{print $4}')"                 % (i*3+2,pb[i], pb[i],i*3-1, pb[i]))
+last_jid1=0
+last_jid2=0
+last_jid3=0
+for run in range(4, 31):
+    for i in range(len(pb)):
+        jid1 = run*1000+i*3
+        jid2 = run*1000+i*3+1
+        jid3 = run*1000+i*3+2
+        if run==4 and i==0:
+            print("jid%d=$(sbatch -o results_evost_XL_run%d/Evo_B_%s.txt --job-name=%s genoa_ML4T.sh --benchmark %s --dataset EXTRALARGE --method EvoTADASHI --use-heuristic| awk '{print $4}')"  % (jid1, run, pb[i], pb[i], pb[i]))
+            print("jid%d=$(sbatch -o results_evost_XL_run%d/Evo_H_%s.txt --job-name=%s genoa_ML4T.sh --benchmark %s --dataset EXTRALARGE --method EvoTADASHI| awk '{print $4}')"                  % (jid2, run, pb[i], pb[i], pb[i]))
+            print("jid%d=$(sbatch -o results_evost_XL_run%d/BeamS_%s.txt --job-name=%s genoa_ML4T.sh --benchmark %s --dataset EXTRALARGE --method BeamSearch| awk '{print $4}')"                  % (jid3, run, pb[i], pb[i], pb[i]))
+        else:
+            print("jid%d=$(sbatch -o results_evost_XL_run%d/Evo_B_%s.txt --job-name=%s --dependency=afterany:$jid%d genoa_ML4T.sh --benchmark %s --dataset EXTRALARGE --method EvoTADASHI --use-heuristic| awk '{print $4}')" % (jid1, run, pb[i], pb[i],last_jid1, pb[i]))
+            print("jid%d=$(sbatch -o results_evost_XL_run%d/Evo_H_%s.txt --job-name=%s --dependency=afterany:$jid%d genoa_ML4T.sh --benchmark %s --dataset EXTRALARGE --method EvoTADASHI| awk '{print $4}')"                 % (jid2, run, pb[i], pb[i],last_jid2, pb[i]))
+            print("jid%d=$(sbatch -o results_evost_XL_run%d/BeamS_%s.txt --job-name=%s --dependency=afterany:$jid%d genoa_ML4T.sh --benchmark %s --dataset EXTRALARGE --method BeamSearch| awk '{print $4}')"                 % (jid3, run, pb[i], pb[i],last_jid3, pb[i]))
+        last_jid1=jid1
+        last_jid2=jid2
+        last_jid3=jid3
