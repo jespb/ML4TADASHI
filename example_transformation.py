158d470c1af1b9622b789540b59f040ea0b3e7cf
jebatista <jesp.batista@gmail.com>
2026-03-26 16:33:37 +0900

    polly

--- example_transformation.py
+++ example_transformation.py
@@ -10,7 +10,7 @@ def print_available_transformations(app):
     """
     Prints available transformations (they might not be legal)
     """
-    for s in [1]: #range(len(app.scops)):
+    for s in range(len(app.scops)):
         st = app.scops[s].schedule_tree
         for n in range(len(st)):
             print("%3d, %3d," %(s, n), st[n].available_transformations)
@@ -31,16 +31,18 @@ print(f"{gemm.user_compiler_options=}")
 
 gemm.compile()
 
-#print(f"{gemm.measure()=}")
+print(f"{gemm.measure()=}")
 
-#oa = str(gemm.dump_arrays())
+oa = str(gemm.dump_arrays())
 
-for tile_size in [64,100]:
+for tile_size in [20, 64]:
     gemm.reset_scops()
 
     sts = gemm.scops[1].schedule_tree
 
     #print("\n\n\n"+ sts[1].yaml_str)
+
+    print_available_transformations(gemm)
 
 
     if translator is None:
@@ -52,7 +54,7 @@ for tile_size in [64,100]:
         # The lists are different across translators
         trs = [
             [1, 2, TrEnum.FULL_SPLIT],
-            [1, 7, TrEnum.TILE_2D, tile_size, tile_size],
+            [1, 7, TrEnum.TILE_3D, tile_size, tile_size, tile_size],
         ]
 
     gemm.transform_list(trs)
@@ -64,9 +66,9 @@ for tile_size in [64,100]:
 
     print(f"{tile_size=} : {tiled.measure()=}")
 
-    #na = str(gemm.dump_arrays())
+    na = str(gemm.dump_arrays())
 
-    #print("Matches original output:", oa==na)
+    print("Matches original output:", oa==na)
 
 
 print("DONE")
