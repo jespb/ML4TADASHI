09b683bbed79c85d3fae1b27d26adbbf73603fc0
Emil VATAI <emil.vatai@gmail.com>
2026-03-28 20:31:33 +0900

    Remove unused set_env

--- pjsub_fugaku.sh
+++ pjsub_fugaku.sh
@@ -8,19 +8,8 @@
 #PJM --mpi "max-proc-per-node=1"
 # #PJM --llio localtmp-size=40Gi
 #PJM -j -S
-set -e
 
-function set_env (){
-  export PATH="$1/bin${PATH:+:${PATH}}"
-  export PATH="$1/bin64${PATH:+:${PATH}}"
-  export LD_LIBRARY_PATH="$1/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
-  export LIBRARY_PATH="$1/lib${LIBRARY_PATH:+:${LIBRARY_PATH}}"
-  export LD_LIBRARY_PATH="$1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
-  export LIBRARY_PATH="$1/lib64${LIBRARY_PATH:+:${LIBRARY_PATH}}"
-  export C_INCLUDE_PATH="$1/include${C_INCLUDE_PATH:+:${C_INCLUDE_PATH}}"
-  export CPLUS_INCLUDE_PATH="$1/include${CPLUS_INCLUDE_PATH:+:${CPLUS_INCLUDE_PATH}}"
-  export MAN_PATH="$1/man${MAN_PATH:+:${MAN_PATH}}"
-}
+set -e
 
 export TMPDIR=/worktmp
 export LD_PRELOAD=/usr/lib/FJSVtcs/ple/lib64/libpmix.so 
