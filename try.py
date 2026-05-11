5b975622f8159e54572ebc30ab34f51c9a38a93a
Emil VATAI <emil.vatai@gmail.com>
2026-05-11 13:23:53 +0900

    More debug code

--- try.py
+++ try.py
@@ -1,5 +1,6 @@
 from tadashi import TrEnum
 from tadashi.apps import Polybench
+from tadashi.scop import TRANSFORMATIONS
 
 app = Polybench("jacobi-1d")
 trs = [
@@ -8,13 +9,20 @@ trs = [
 ]
 
 app.transform_list(trs)
-node = app.scops[0].schedule_tree[2]
-print(node.yaml_str)
+node = app.scops[0].schedule_tree[3]
+tr = TrEnum.FUSE
+T = TRANSFORMATIONS[tr]
+
+# print(node.yaml_str)
 
 a = node.available_transformations
-print(a)
+print(f"{tr in a=}")
+print(f"{T.valid(node)=}")
 
-print(f"{node.get_args(TrEnum.FUSE, -10, 10)=}")
+args = node.get_args(tr, -10, 10)
+print(f"{args=}")
 
-app.transform_list(trs)
-tapp = app.generate_code()
+trs = [[0, 3, tr, -10, 10]]
+
+# app.transform_list(trs)
+# tapp = app.generate_code()
